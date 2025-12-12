f65792c294fadd0a011adcb564e4c6bd41068a25
rm1300 <232829235+rm1300@users.noreply.github.com>
2025-12-12 18:53:11 +0100

    Fix deploying

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,8 @@ import createMDX from "@next/mdx";
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: "export",
+  basePath: process.env.PAGES_BASE_PATH,
   pageExtensions: ["js", "jsx", "md", "mdx", "ts", "tsx"],
 };
 
