3ba4900cc823397ea712743494666763b2695099
rm1300 <232829235+rm1300@users.noreply.github.com>
2025-12-12 18:56:25 +0100

    ...

--- next.config.mjs
+++ next.config.mjs
@@ -2,14 +2,11 @@ import createMDX from "@next/mdx";
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: "export",
+  basePath: process.env.PAGES_BASE_PATH,
   pageExtensions: ["js", "jsx", "md", "mdx", "ts", "tsx"],
 };
 
-const withMDX = createMDX({
-  extension: /\.(md|mdx)$/,
-});
+const withMDX = createMDX({});
 
-export default Object.assign(withMDX(nextConfig), {
-  output: "export",
-  basePath: process.env.PAGES_BASE_PATH,
-});
+export default withMDX(nextConfig);
